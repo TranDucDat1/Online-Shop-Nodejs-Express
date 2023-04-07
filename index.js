048a4cbaa993df9e1988f43d87906fe658690008
Duc Dat <datnho53@gmail.com>
2023-04-07 23:36:37 +0700

    install nodemon

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('ok');
+    var a = 1;
+    var b = 1;
+    c = a + b;
+    res.send(` ${c}ok`);
 })
 
 app.listen(port, () => {
